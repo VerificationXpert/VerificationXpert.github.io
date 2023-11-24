f8b056f17a233e5391a12e5ca8dba92e379ffe3a
silicon <srinivas.siripurapu@outlook.com>
2023-11-24 22:59:06 +0530

    Workflow added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,6 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
-});
+  site: 'https://VerificationXpert.github.io',
+  base: '/',
+})
